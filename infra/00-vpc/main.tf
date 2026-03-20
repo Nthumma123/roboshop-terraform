39236c99ffc0e93bbaf46b534ec6dc8fdf994e4c
neeli <neelimareddygopu1@gmail.com>
2026-03-20 13:56:41 +0530

    actions

--- infra/00-vpc/main.tf
+++ infra/00-vpc/main.tf
@@ -1,6 +1,8 @@
-module "vpc"{
-    source = "git::https://github.com/Nthumma123/roboshop-infra-dev.git//modules/terraform-aws-vpc?ref=main"
+
+
+module "vpc" {
+    source = "git::https://github.com/Nthumma123/roboshop-terraform.git//modules/terraform-aws-vpc?ref=main"
     project = var.project
     environment = var.environment
+    #is_peering_required = true
 }
-
