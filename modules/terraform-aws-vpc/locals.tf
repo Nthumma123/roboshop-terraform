efb54af21d92c975a838f5f8dd1a01fa36603b37
neeli <neelimareddygopu1@gmail.com>
2026-03-21 21:45:35 +0530

    actions

--- modules/terraform-aws-vpc/locals.tf
+++ modules/terraform-aws-vpc/locals.tf
@@ -9,12 +9,12 @@ locals {
         Name = "${var.project}-${var.environment}"
         } ,
     var.vpc_tags)
+    
     igw_final_tags = merge(local.common_tags,
         {
             Name = "${var.project}-${var.environment}-igw"
         } ,
     var.igw_tags)
     az_names = slice(data.aws_availability_zones.available.names,0,2)
-
 
 }
